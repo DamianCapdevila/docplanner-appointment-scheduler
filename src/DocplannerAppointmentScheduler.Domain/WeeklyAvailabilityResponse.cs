97d115efc46bb80334ee3f0e8dfd69ec81547a8b
Damian Capdevila <damian.capdevila@hotmail.com>
2024-10-05 19:37:20 +0200

    (chore): remove folder that does not make sense from the domain.

--- src/DocplannerAppointmentScheduler.Domain/WeeklyAvailabilityResponse.cs
+++ src/DocplannerAppointmentScheduler.Domain/WeeklyAvailabilityResponse.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DocplannerAppointmentScheduler.Domain.DTOs
+namespace DocplannerAppointmentScheduler.Domain
 {
     public class WeeklyAvailabilityResponse
     {
